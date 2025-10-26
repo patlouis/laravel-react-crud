905749573fed74d938b5ad37467b3b8b14cf839c
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-10-26 21:34:58 +0800

    feat: add update product

--- resources/js/pages/products/edit.tsx
+++ resources/js/pages/products/edit.tsx
@@ -14,66 +14,85 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Create() {
-    const { data, setData, post, processing, errors } = useForm({
-        name: '',
-        category: '',
-        description: '',
-        price: '',
+interface Product {
+    id: number;
+    name: string;
+    category: string;
+    description: string;
+    price: number | string;
+}
+
+interface Props {
+    product: Product;
+}
+
+export default function Create({ product }: Props) {
+    const { data, setData, put, processing, errors } = useForm({
+        name: product.name,
+        category: product.category,
+        description: product.description,
+        price: product.price,
     });
 
-    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    function handleUpdate(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        post(route('products.update'));
+        put(route('products.update', product.id));
     }
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Create Product" />
+            <Head title="Edit Product" />
             <div className="w-8/12 p-4">
-                <form onSubmit={handleSubmit} className="space-y-4">
+                <form onSubmit={handleUpdate} className="space-y-4">
                     <div className="flex flex-col gap-1.5">
                         <Label>Name:</Label>
-                        <Input 
-                            type="text" 
+                        <Input
+                            type="text"
                             placeholder="Enter name"
                             value={data.name}
-                            onChange={(e) => setData('name', e.target.value)} 
+                            onChange={(e) => setData('name', e.target.value)}
                         />
                     </div>
                     <div className="flex flex-col gap-1.5">
                         <Label>Category:</Label>
-                        <Input 
-                            type="text" 
+                        <Input
+                            type="text"
                             placeholder="Enter category"
-                            value={data.category} 
-                            onChange={(e) => setData('category', e.target.value)} 
+                            value={data.category}
+                            onChange={(e) =>
+                                setData('category', e.target.value)
+                            }
                         />
                     </div>
                     <div className="flex flex-col gap-1.5">
                         <Label>Description:</Label>
-                        <Input 
-                            type="text" 
+                        <Input
+                            type="text"
                             placeholder="Enter description"
                             value={data.description}
-                            onChange={(e) => setData('description', e.target.value)} 
+                            onChange={(e) =>
+                                setData('description', e.target.value)
+                            }
                         />
                     </div>
                     <div className="flex flex-col gap-1.5">
                         <Label>Price:</Label>
-                        <Input 
-                            type="number" 
+                        <Input
+                            type="number"
                             placeholder="Enter price"
-                            value={data.price} 
-                            onChange={(e) => setData('price', e.target.value)} 
+                            value={data.price}
+                            onChange={(e) =>
+                                setData('price', e.target.value)
+                            }
                         />
                     </div>
                     <div>
-                        <Button 
-                            type ="submit" 
-                            disabled={processing} 
-                            className="cursor-pointer">
-                                {processing ? 'Saving...' : 'Save'}
+                        <Button
+                            type="submit"
+                            disabled={processing}
+                            className="cursor-pointer"
+                        >
+                            {processing ? 'Saving...' : 'Save'}
                         </Button>
                     </div>
                 </form>
