c604b62b582f0eba383baee92e287d608690a81e
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-10-26 12:48:56 +0800

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+use App\Http\Requests\StoreProductRequest;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = Product::all();
+        return Inertia::render('products/index', [ 'products' => $products ]);
+    }
+
+    public function create() {
+        return Inertia::render('products/create', []);
+    }
+
+    public function store(StoreProductRequest $request) {
+        $data = $request->validated();
+        Product::create($data);
+        return redirect()->route('products.index')->with('message', 'Product created successfully.');
+    }
+
+    public function edit(Product $product) {
+        return Inertia::render('products/edit', [ 'product' => $product ]);
+    }
+}
---
+++ resources/js/pages/products/index.tsx
@@ -0,0 +1,90 @@
+import { Button } from '@/components/ui/button';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link } from '@inertiajs/react';
+import { route } from 'ziggy-js';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+
+interface Product {
+    id: number;
+    name: string;
+    category: string;
+    description: string;
+    price: number;
+}
+
+interface Props {
+    products: Product[];
+}
+
+export default function index({ products }: Props) {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Products" />
+            <div className="m-6">
+                <div className="mb-4 flex justify-end">
+                    <Link href={route('products.create')}>
+                        <Button className="cursor-pointer">
+                            Create Product
+                        </Button>
+                    </Link>
+                </div>
+                <Table>
+                    <TableCaption>A list of your products.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>ID</TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Category</TableHead>
+                            <TableHead>Description</TableHead>
+                            <TableHead>Price</TableHead>
+                            <TableHead>Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {products.length > 0 ? (
+                            products.map((product) => (
+                                <TableRow key={product.id}>
+                                    <TableCell>{product.id}</TableCell>
+                                    <TableCell>{product.name}</TableCell>
+                                    <TableCell>{product.category}</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                    <TableCell>{product.price}</TableCell>
+                                    <TableCell>
+                                        <Button className="cursor-pointer">
+                                            Edit
+                                        </Button>
+                                        <Button className="ml-2 cursor-pointer">
+                                            Delete
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={6} className="text-center">
+                                    No products found.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+        </AppLayout>
+    );
+}
